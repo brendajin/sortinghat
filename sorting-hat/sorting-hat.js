2c966fd48a833f30a880a8981277098ae86bb235
Lillian Chan <californium98@gmail.com>
2013-04-13 15:24:19 -0700

    more templates and events from Brenda and Lillian

--- sorting-hat/sorting-hat.js
+++ sorting-hat/sorting-hat.js
@@ -1,4 +1,5 @@
 Events = new Meteor.Collection("events");
+Participants = new Meteor.Collection("participants");
 
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
@@ -9,11 +10,23 @@ if (Meteor.isClient) {
     return Events.find({}, {sort: {score: -1, name: 1}});
   };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You selected Event 1");
+  Template.allParticipants.participants = function () {
+    return Participants.find({}, {sort: {name:1}});
+  }
+
+  Template.newEventForm.events({
+    'click #maketeams': function() {
+      var eventName = $('#newEventName').val();
+      Events.insert({name: eventName});
+      $('#newEventName').val('');
+    }
+  });
+
+  Template.newParticipantsForm.events({
+    'click #signUp': function() {
+      var firstName = $('#firstName').val();
+      Participants.insert({name: firstName});
+      $('#firstName').val('');
     }
   });
 }
@@ -22,9 +35,9 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
     if (Events.find().count() === 0) {
-      var events = ["hackathon",
-                   "soccer",
-                   "birthday"];
+      var events = ["CM hackathon",
+                   "Hawks vs Raptors Soccer Game",
+                   "Dana's Sweet 16"];
       for (var i = 0; i < events.length; i++)
         Events.insert({name: events[i], score: Math.floor(Random.fraction()*10)*5});
     }
